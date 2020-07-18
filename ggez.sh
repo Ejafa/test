2466c7a32f2523445eb7ad41f7aab909bb48de2e
Ejafa <Ejafa@users.noreply.github.com>
2020-07-18 20:42:52 +0800

    Update ggez.sh

--- ggez.sh
+++ ggez.sh
@@ -37,15 +37,15 @@ elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
     release="centos"
     systemPackage="yum"
     systempwd="/usr/lib/systemd/system/"
-be
+fi
 
 change_panel(){
 if test -s /etc/systemd/system/trojan-web.service; then
 	green " "
 	green " "
-	green "=================================================="
-	 blue "Trojan panel service detected, start configuration"
-	green "=================================================="
+	green "======================================================="
+	 blue "  Trojan panel service detected, start configuration"
+	green "======================================================="
 	sleep 2s
 	$systemPackage update -y
 	$systemPackage -y install nginx unzip curl wget
@@ -54,24 +54,24 @@ if test -s /etc/systemd/system/trojan-web.service; then
 if test -s /etc/nginx/nginx.conf; then
 	rm -rf /etc/nginx/nginx.conf
   wget -P /etc/nginx https://raw.githubusercontent.com/V2RaySSR/Trojan_panel_web/master/nginx.conf
-	green "========================================================"
-	blue "      Please enter the domain name bound by Trojan "
-	green "========================================================"
+	green "================================="
+	blue "     请输入Trojan绑定的域名"
+	green "================================="
 	read your_domain
   sed -i "s/localhost/$your_domain/;" /etc/nginx/nginx.conf
 	green " "
-	green "================================================================================="
-	 blue "     Start downloading and deploying the source code of the disguised site "
-	green "================================================================================="
+	green "====================================="
+	 blue "    Please enter the domain name:"
+	green "====================================="
 	sleep 2s
 	rm -rf /usr/share/nginx/html/*
 	cd /usr/share/nginx/html/
 	wget https://github.com/V2RaySSR/Trojan/raw/master/web.zip
 	unzip web.zip
 	green " "
-	green "==========================================="
-	blue "        Start configuring trojan-web "
-	green "==========================================="
+	green "========================================="
+	blue "       Start configuring of trojan-web"
+	green "========================================="
 	sleep 2s
   sed -i '/ExecStart/s/trojan web -p 81/trojan web/g' /etc/systemd/system/trojan-web.service
   sed -i '/ExecStart/s/trojan web/trojan web -p 81/g' /etc/systemd/system/trojan-web.service
@@ -81,30 +81,30 @@ if test -s /etc/nginx/nginx.conf; then
   green " "
   green " "
   green " "
-	green "=============================================================================="
+	green "======================================================================="
 	green " "
-	 blue "   WIN / MAC universal client download, more introduction to this script "
+	 blue "  WIN / MAC universal client download, more introduction to this script"
 	 blue "  https://www.v2rayssr.com/trojanpanel.html "
 	green " "
-	 blue "   Script exchange telegram group: https://goii.cc/tg "
+	 blue "  Script sharing Telegram Group：https://goii.cc/tg"
 	green " "
-	 blue "   Disguise site directory /usr/share/nginx/html "
-	 blue "   Panel management address http:// $your_domain :666 "
-	green "==============================================================================="
+	 blue "  Disguise Site Directory: /usr/share/nginx/html "
+	 blue "  Panel management address http://$your_domain:666 "
+	green "======================================================================="
 else
-	green "==============================================================="
-	  red "      Nginx is not installed correctly, please try again "
-	green "==============================================================="
+	green "=================================================="
+	  red "Nginx is not installed correctly, please try again"
+	green "=================================================="
 	sleep 2s
 	exit 1
-be
+fi
 else
-	green "========================================="
-	  red "The Trojan Panel Service is not detected "
-	green "========================================="
+	green "================================="
+	  red "Trojan panel service not detected"
+	green "================================="
 	sleep 2s
 	exit 1
-be
+fi
 }
 
 bbr_boost_sh(){
@@ -119,22 +119,22 @@ trojan_install(){
 
 start_menu(){
   clear
+	green "================================================================="
+   blue "This script supports: Debian9+ / Ubuntu16.04+ / Centos7+"
+	 blue "Website: www.v2rayssr.com (Access denied from China)"
+	 blue " YouTube Channel：波仔分享"
+	 blue "This script is forbidden to be reproduced on any website in China"
+	green "================================================================="
+   blue "Introduction: One-click to change the Trojan-Panel panel port and set up a fake site""
 	green "=========================================================="
-   blue " This script supports: Debian9+ / Ubuntu16.04+ / Centos7+ "
-	 blue " Website: www.v2rayssr.com (Domestic access is prohibited) "
-	 blue " YouTube Channel: Pozai Sharing "
-	 blue " This script is forbidden to be reproduced on any website in China "
+	  red "Before running this script, please make sure you have installed Jrohy's panel program"
 	green "=========================================================="
-   blue " Introduction: One-click to change the Trojan-Panel panel port and set up a fake site "
-	green "=========================================================="
-	  red " Before running this script, please make sure you have installed Jrohy's panel program "
-	green "=========================================================="
-	 blue " 1. Trojan multi-user management deployment program of Jrohy Great God "
-   blue " 2. Change the Trojan panel port and set up a fake site "
-   blue " 3. Install BBRPlus4 in one acceleration "
-   blue " 0. Exit the script "
+	 blue "1. Trojan multi-user management deployment program of Jrohy"
+   blue "2. Change the Trojan panel port and set up a fake site"
+   blue "3. Install BBRPlus4 all-in-one acceleration"
+   blue " 0. Exit the script"
     echo
-    read -p " Please enter a number: " num
+    read -p "Please enter the number:" num
     case "$num" in
     1)
 		trojan_install
@@ -150,7 +150,7 @@ start_menu(){
 		;;
 		*)
 	clear
-	echo  " Please enter the correct number "
+	echo "Please enter the correct number"
 	sleep 2s
 	start_menu
 	;;
